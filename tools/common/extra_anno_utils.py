6a24a764bfd539c9ac76251936484570613dcc2f
Tribhuvanesh Orekondy <tribhuvanesh@gmail.com>
2017-09-27 18:27:50 +0200

    Load image to text

--- tools/common/extra_anno_utils.py
+++ tools/common/extra_anno_utils.py
@@ -35,3 +35,24 @@ def bb_to_verts(bb):
     vrts = [[d.get('x', 0), d.get('y', 0)] for d in vrts_dct]  # Convert to [ [x1, y1], [x2, y2], ..]
     vrts.append(vrts[0])  # Reconnect to first vertex
     return np.ndarray.flatten(np.asarray(vrts))
+
+
+def load_image_id_to_text():
+    index_path = osp.join(SEG_ROOT, 'privacy_filters', 'cache', 'image_id_text_index.pkl')
+    if osp.exists(index_path):
+        print 'Found cached img-text index. Loading it...'
+        return pickle.load(open(index_path, 'rb'))
+    else:
+        print 'Creating img-text index ...'
+        img_text_index = dict()
+        for fold in ['train2017', 'val2017', 'test2017', ]:
+            fold_dir = osp.join(EXTRA_ANNO_PATH, fold)
+            for filename in os.listdir(fold_dir):
+                image_id, _ = osp.splitext(filename)
+                with open(osp.join(fold_dir, filename)) as jf:
+                    eanno = json.load(jf)
+                    if 'fullTextAnnotation' in eanno:
+                        this_text = eanno['fullTextAnnotation']['text']
+                        img_text_index[image_id] = this_text
+        pickle.dump(img_text_index, open(index_path, 'wb'))
+        return img_text_index
